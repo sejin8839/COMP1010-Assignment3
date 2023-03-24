1f62e9a2d2e5dcb978896d9899f73eca88496435
sejin8839 <sejinon01@gmail.com>
2023-03-24 16:30:12 +0900

    Create A3Turtle.py

---
+++ A3/A3Turtle.py
@@ -0,0 +1,56 @@
+# A2 Turtle Assignment
+# Starter code by David Johnson
+# For COMP1010 University of Utah
+#
+# Assignment written by: __________________
+
+# Use the turtle module
+import turtle
+
+# Create a screen to draw on
+window = turtle.Screen()
+
+# A2 a)
+# Make a turtle named triangle and use that turtle to draw
+# a triangle with one corner at position x = 0 and y = 0
+
+
+
+done()
+
+
+# A2 b)
+# Make a turtle named zigzag and use that turtle to draw
+# a blue zigzag shape starting at position x = -100 and y = 100.
+# See the assignment for an example picture. You must use a
+# loop to repeat the pattern for 8 peaks.
+# The turtle should not leave any other lines except the zig zag.
+
+
+done()
+
+# A2 c)
+# Make a turtle named spiral and use that turtle to draw
+# a green square spiral shape with the center starting at
+# position x = 0 and y = -100.
+# See the assignment for an example picture.
+# You must use a loop to grow the spiral.
+# The turtle should not leave any other lines except the spiral.
+
+import turtle
+spiral = turtle.Turtle()
+turtle.color('green')
+turtle.setup(0, -100)
+
+
+
+done()
+
+
+# A2 d)
+# Add a new turtle. Draw a nice picture of your own design.
+# There should be different colors and sections that require
+# the pen to go back up and back down.
+
+# Leave the window up until it is clicked
+window.exitonclick()
