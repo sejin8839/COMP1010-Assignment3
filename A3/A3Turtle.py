5acd137804aeee7d70dfd423714f2010e0163a01
sejin8839 <sejinon01@gmail.com>
2023-03-24 20:25:30 +0900

    Update A3Turtle.py

--- A3/A3Turtle.py
+++ A3/A3Turtle.py
@@ -2,7 +2,7 @@
 # Starter code by David Johnson
 # For COMP1010 University of Utah
 #
-# Assignment written by: __________________
+# Assignment written by: Sejin Yoon, u1311019
 
 # Use the turtle module
 import turtle
@@ -10,23 +10,22 @@ import turtle
 # Create a screen to draw on
 window = turtle.Screen()
 
+
+
 # A2 a)
 # Make a turtle named triangle and use that turtle to draw
 # a triangle with one corner at position x = 0 and y = 0
+
 
 import turtle
 window = turtle.Screen()
 triangle = turtle.Turtle()
 triangle.width(2)
 triangle.goto(0,0)
-triangle.dot()
 triangle.goto(200,0)
-triangle.dot()
 triangle.goto(100,200)
-triangle.dot()
 triangle.goto(0,0)
-
-
+turtle.done()
 
 
 
@@ -38,8 +37,21 @@ triangle.goto(0,0)
 # The turtle should not leave any other lines except the zig zag.
 
 
-
-
+import turtle
+window = turtle.Screen()
+zigzag = turtle.Turtle()
+zigzag.color('blue')
+zigzag.penup()
+zigzag.goto(-100,100)
+zigzag.pendown()
+zigzag.width(1)
+zigzag.left(60)
+for _ in range(8):
+    zigzag.forward(50)
+    zigzag.right(120)
+    zigzag.forward(50)
+    zigzag.left(120)
+turtle.done()
 
 
 
@@ -51,22 +63,57 @@ triangle.goto(0,0)
 # You must use a loop to grow the spiral.
 # The turtle should not leave any other lines except the spiral.
 
+
 import turtle
 window = turtle.Screen()
 spiral = turtle.Turtle()
 spiral.color('green')
-spiral.setup(0, -100)
+spiral.penup()
+spiral.goto(0,-100)
+spiral.pendown()
 spiral.width(1)
 for x in range(1,200,5):
     spiral.forward(x)
     spiral.left(90)
-spiral.exitonclick()
+turtle.done()
+
 
 
 # A2 d)
 # Add a new turtle. Draw a nice picture of your own design.
 # There should be different colors and sections that require
 # the pen to go back up and back down.
-
 # Leave the window up until it is clicked
-window.exitonclick()
+
+
+color = ["red","purple","blue","green","orange","yellow"]
+colorIndex = 0
+
+import turtle
+window = turtle.Screen()
+turtle.bgcolor("black")
+Apple = turtle.Turtle()
+Apple.speed(10)
+Apple.shape("circle")
+Apple.color('red')
+Apple.penup()
+Apple.goto(50,50)
+Apple.pendown()
+appleWidth = 3
+dotSize = 2
+for x in range(1, 200, 3):
+    Apple.pencolor(color[colorIndex%6])
+    Apple.forward(x)
+    Apple.left(120)
+    Apple.dot(dotSize,"black")
+    Apple.right(60)
+    Apple.dot(dotSize,"black")
+    Apple.width(appleWidth)
+    colorIndex += 1
+
+    # It increases width AND dotSize in every loop
+    appleWidth += 0.1
+    dotSize += 2
+turtle.exitonclick()
+
+
